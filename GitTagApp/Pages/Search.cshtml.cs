7ac35a6c1aaf09b3b6b0a93abb980ecf476faa82
Eduardo <lodi001@uol.com.br>
2020-08-31 21:24:38 -0300

    Finished adding search feature

---
+++ GitTagApp/Pages/Search.cshtml.cs
@@ -0,0 +1,104 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GitTagApp.Entities;
+using GitTagApp.Interfaces;
+using GitTagApp.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using Octokit;
+using Octokit.Internal;
+using User = GitTagApp.Entities.User;
+
+namespace GitTagApp.Pages
+{
+    public class SearchModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        
+        private readonly IGitRepoService _repoService;
+        
+        private readonly IUserService _userService;
+        
+        private readonly ITagService _tagService;
+
+        private readonly IGitRepoTagService _gitRepoTagService;
+        public IReadOnlyList<Repository> Repositories { get; set; }
+        public IReadOnlyList<Repository> StarredRepos { get; set; }
+        public User UserEntity { get; set; }
+        public List<GitRepo> GitRepos { get; set; }
+
+        [BindProperty]
+        public Tag Tag { get; set; }
+
+        public SearchModel(ILogger<IndexModel> logger, IGitRepoService gitRepoService, ITagService tagService, IGitRepoTagService gitRepoTagService, IUserService userService)
+        {
+            _logger = logger;
+            _repoService = gitRepoService;
+            _tagService = tagService;
+            _gitRepoTagService = gitRepoTagService;
+            _userService = userService;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task OnPost()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var gitName = string.Empty;
+                string accessToken = await HttpContext.GetTokenAsync("access_token");
+                var github = new GitHubClient(new ProductHeaderValue("AspNetCoreGitHubAuth"), 
+                    new InMemoryCredentialStore(new Credentials(accessToken)));
+                
+                Repositories = await github.Repository.GetAllForCurrent();
+            
+                StarredRepos = await github.Activity.Starring.GetAllForCurrent();
+                
+                var tempUser = await github.User.Get(Repositories[0].Owner.Login);
+                gitName = tempUser.Name;
+                            
+                UserEntity = new Entities.User
+                {
+                    Id = Repositories[0].Owner.Id,
+                    Name = gitName
+                };
+            }
+            var outRepos = new List<GitRepo>();
+            var searchString = Request.Form["Tag.Name"];
+            var returnRepos = (from gt in _gitRepoTagService.GetAll()
+                join t in _tagService.GetAll()
+                    on gt.TagId equals t.Id
+                where t.Name.ToLower().Contains(searchString)
+                select gt).ToList();
+
+            var edu = (from r in StarredRepos
+                join gt in returnRepos
+                    on r.Id equals gt.GitRepoId
+                select r).ToList();
+
+            foreach (var repo in edu)
+            {
+                var dbRepo = new GitRepo
+                {
+                    Id = repo.Id,
+                    Name = repo.Name,
+                    Description = repo.Description,
+                    HttpUrl = repo.Url,
+                    UserId = UserEntity.Id,
+                };
+                    
+                var repoBuilder = new RepoBuilder(_userService, _tagService, _gitRepoTagService);
+                var outputRepo = repoBuilder.GetPayload(dbRepo);
+                outRepos.Add(outputRepo);
+            }
+
+            GitRepos = outRepos;
+        }
+    }
+}
