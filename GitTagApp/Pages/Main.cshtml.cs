a1ad56b7f6f7771b9206af951802cf96642a2e56
Eduardo <lodi001@uol.com.br>
2020-09-10 18:02:02 -0300

    Added feature to clean up database when the user un-stars a previously starred repository

--- GitTagApp/Pages/Main.cshtml.cs
+++ GitTagApp/Pages/Main.cshtml.cs
@@ -1,5 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Castle.Core.Internal;
+using GitTagApp.Entities;
+using GitTagApp.Interfaces;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Octokit;
+using Octokit.Internal;
 
 namespace GitTagApp.Pages
 {
@@ -9,13 +19,44 @@ namespace GitTagApp.Pages
 
         private readonly ILogger<ErrorModel> _logger;
 
-        public MainModel(ILogger<ErrorModel> logger)
+        public IReadOnlyList<Repository> Repositories { get; set; }
+        public IReadOnlyList<Repository> StarredRepos { get; set; }
+        private readonly IGitRepoTagService _gitRepoTagService;
+
+        public MainModel(ILogger<ErrorModel> logger, IGitRepoTagService gitRepoTagService)
         {
             _logger = logger;
+            _gitRepoTagService = gitRepoTagService;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                var removeTag = new List<GitRepoTag>();
+                string accessToken = await HttpContext.GetTokenAsync("access_token");
+                var github = new GitHubClient(new ProductHeaderValue("AspNetCoreGitHubAuth"),
+                    new InMemoryCredentialStore(new Credentials(accessToken)));
+
+                Repositories = await github.Repository.GetAllForCurrent();
+
+                StarredRepos = await github.Activity.Starring.GetAllForCurrent();
+
+                var isStillStarred = _gitRepoTagService.GetAll().ToList();
+
+                removeTag.AddRange(
+                    (from rt in isStillStarred
+                        join sr in StarredRepos on rt.GitRepoId !equals sr.Id
+                        select rt).ToList());
+
+                if (!removeTag.IsNullOrEmpty())
+                {
+                    foreach (var item in removeTag)
+                    {
+                        var result = _gitRepoTagService.DeleteRepoTag(item.GitRepoId, item.TagId);
+                    }
+                }
+            }
         }
     }
 }
